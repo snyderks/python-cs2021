2cb8fbb759e90737ff283a2c7beba166ad274f21
Kristian Snyder <knsnyder96@gmail.com>
2017-01-12 14:44:00 -0500

    Completed extra questions for Lab 1

--- labs/lab01.py
+++ labs/lab01.py
@@ -69,6 +69,68 @@ def fib(n):
             prev = temp
         return curr
 
+# Q11
+def is_factor(x, y):
+    """ Returns True if x is a factor of y, False otherwise.
+
+    >>> is_factor(3, 6)
+    True
+    >>> is_factor(4, 10)
+    False
+    >>> is_factor(0, 5)
+    False
+    >>> is_factor(0, 0)
+    False
+    """
+    "*** YOUR CODE HERE ***"
+    return x > 0 and y % x == 0
+
+# Q12
+def gets_discount(x, y):
+    """ Returns True if this is a combination of a senior citizen
+    and a child, False otherwise.
+
+    >>> gets_discount(65, 12)
+    True
+    >>> gets_discount(9, 70)
+    True
+    >>> gets_discount(40, 45)
+    False
+    >>> gets_discount(40, 75)
+    False
+    >>> gets_discount(65, 13)
+    False
+    >>> gets_discount(7, 9)
+    False
+    >>> gets_discount(73, 77)
+    False
+    >>> gets_discount(70, 31)
+    False
+    >>> gets_discount(10, 25)
+    False
+    """
+    "*** YOUR CODE HERE ***"
+    SENIOR = 65
+    CHILD = 12
+    return (x >= SENIOR and y <= CHILD) or (x <= CHILD and y >= SENIOR)
+
+# Q13
+def falling(n, k):
+    """Compute the falling factorial of n to depth k.
+
+    >>> falling(6, 3)  # 6 * 5 * 4
+    120
+    >>> falling(4, 0)
+    1
+    >>> falling(4, 3)  # 4 * 3 * 2
+    24
+    >>> falling(4, 1)  # 4
+    4
+    """
+    "*** YOUR CODE HERE ***"
+    from math import factorial
+    return int(factorial(n) / factorial(n-k))
+
 if __name__ == "__main__":
     import doctest
     doctest.testmod()
